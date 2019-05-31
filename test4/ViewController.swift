215bbfee19e9ed7f3ad700407f1aaffbf5ffae54
JoeBarkley <joeybarkley100@gmail.com>
2019-05-31 15:18:20 -0500

    We ddosed them hard

--- test4/ViewController.swift
+++ test4/ViewController.swift
@@ -9,23 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    let tenArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     var imageViewArray: [UIImage] = [UIImage(named: "green")!, UIImage(named: "purple")!, UIImage(named: "patty")!]
-    var cageArray: [UIImage]!
-    var kittenArray: [UIImage]!
-    var murrayArray: [UIImage]!
-    var imageArrayArray : [[UIImage]]!
+    var cageArray: [UIImage] = []
+    var kittenArray: [UIImage] = []
+    var murrayArray: [UIImage] = []
+    var imageArrayArray: [[UIImage]] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("view loaded")
-        collectionView.reloadData()
-    }
 
+        addCageArray()
+//        addKittenArray()
+//        addMurrayArray()
+        
+        imageArrayArray.append(cageArray)
+//        imageArrayArray.append(murrayArray)
+//        imageArrayArray.append(kittenArray)
+        
+    }
+    
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        print("hello")
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -33,53 +39,67 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageViewArray.count
+        return cageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! CollectionViewCell
-        cell.imageView.image = imageViewArray[indexPath.row]
-        print(indexPath.section)
+        let usedArray = cageArray
+        cell.imageView.image = usedArray[indexPath.row]
+        print("Goodbye")
         return cell
     }
-    func addCageArray()
-    {
-        let random1 = Int.random(in: 100...500)
-        let random2 = Int.random(in: 100...500)
-        let url = URL(string: "https://www.placecage.com/c/\(random1)/\(random2)")
-        let session = URLSession.shared
-        let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
-            DispatchQueue.main.async {
-                self.cageArray.append(UIImage(data: data!)!)
+    
+    func addCageArray() {
+        for item in 1...10{
+            let random1 = Int.random(in: 100...500)
+            let random2 = Int.random(in: 100...500)
+            let url = URL(string: "https://www.placecage.com/c/\(random1)/\(random2)")
+            let session = URLSession.shared
+            let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
+                DispatchQueue.main.async {
+                    let image = (UIImage(data: data!)!)
+                    self.cageArray.append(image)
+                    print("hello")
+                    self.collectionView.reloadData()
+                }
             }
+            task.resume()
         }
     }
     
-    func addKittenArray()
-    {
-        let random1 = Int.random(in: 100...500)
-        let random2 = Int.random(in: 100...500)
-        let url = URL(string: "https://www.placecage.com/c/\(random1)/\(random2)")
-        let session = URLSession.shared
-        let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
-            DispatchQueue.main.async {
-                self.kittenArray.append(UIImage(data: data!)!)
-            }
-        }
-    }
+//    func addKittenArray() {
+//        for item in tenArray{
+//            let random1 = Int.random(in: 100...500)
+//            let random2 = Int.random(in: 100...500)
+//            let url = URL(string: "http://placekitten.com/\(random1)/\(random2)")
+//            print(url)
+//            let session = URLSession.shared
+//            let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
+//                DispatchQueue.main.async {
+//                    let image = (UIImage(data: data!)!)
+//                    self.kittenArray.append(image)
+//                }
+//            }
+//            task.resume()
+//        }
+//    }
     
-    func addMurrayArray()
-    {
-        let random1 = Int.random(in: 100...500)
-        let random2 = Int.random(in: 100...500)
-        let url = URL(string: "https://www.placecage.com/c/\(random1)/\(random2)")
-        let session = URLSession.shared
-        let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
-            DispatchQueue.main.async {
-                self.murrayArray.append(UIImage(data: data!)!)
-            }
-        }
-    }
-
+//    func addMurrayArray() {
+//        for item in tenArray{
+//            let random1 = Int.random(in: 100...500)
+//            let random2 = Int.random(in: 100...500)
+//            let url = URL(string: "https://fillmurray.com/\(random1)/\(random2)")
+//            let session = URLSession.shared
+//            let task = session.dataTask(with: url!) { (data: Data?, response: URLResponse?, error: Error?) in
+//                DispatchQueue.main.async {
+//                    let image = (UIImage(data: data!)!)
+//                    self.murrayArray.append(image)
+//                }
+//            }
+//            task.resume()
+//        }
+//    }
+    
 }
 
